6faad67ae72522ce214d79c20b18c79777acdcb8
hemaher0 <hemahero@naver.com>
2026-06-30 01:32:36 +0900

    Tune initial git prompt latency

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -482,6 +482,10 @@
   # config: `git config bash.showDirtyState false`.
   typeset -g POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY=-1
 
+  # Wait a little longer for the initial gitstatus response on MSYS2 so the first prompt
+  # usually includes the VCS segment instead of popping it in asynchronously.
+  typeset -g POWERLEVEL9K_VCS_MAX_SYNC_LATENCY_SECONDS=0.2
+
   # Don't show Git status in prompt for repositories whose workdir matches this pattern.
   # For example, if set to '~', the Git repository at $HOME/.git will be ignored.
   # Multiple patterns can be combined with '|': '~(|/foo)|/bar/baz/*'.
